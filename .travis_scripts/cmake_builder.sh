fbab5f05e152853a472cc4ed343a098062d26aef
chenguoping <chenguopingdota@163.com>
2020-04-22 21:04:22 +0800

    update cmake_builder.sh

--- .travis_scripts/cmake_builder.sh
+++ .travis_scripts/cmake_builder.sh
@@ -98,6 +98,13 @@ else
   export _BUILD_EXE=make
 fi
 
+# Language standard
+if [ "${LANGUAGE_STANDARD}" == "11" ]; then
+  _BUILD_STANDARD=ON
+else
+  _BUILD_STANDARD=OFF
+fi
+
 _BUILD_DIR_NAME="build-cmake_${BUILD_TYPE}_${LIB_TYPE}_${_COMPILER_NAME}_${_BUILD_EXE}"
 mkdir -p ${_BUILD_DIR_NAME}
 cd "${_BUILD_DIR_NAME}"
@@ -112,6 +119,7 @@ cd "${_BUILD_DIR_NAME}"
     -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} \
     -DBUILD_SHARED_LIBS:BOOL=${_CMAKE_BUILD_SHARED_LIBS} \
     -DCMAKE_INSTALL_PREFIX:PATH=${DESTDIR} \
+    -DBUILD_WITH_CXX_11="${_BUILD_STANDARD}" \
     ../
 
   ctest -C ${BUILD_TYPE} -D ExperimentalStart -D ExperimentalConfigure -D ExperimentalBuild ${CTEST_TESTING_OPTION} -D ExperimentalSubmit
